c7c68ddd1a1407410bf058c602323956df14d4f1
@ignasius <igniz87@gmail.com>
2019-10-18 10:29:11 +0700

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+var ig = require('instagram-scraping')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+const TelegramBot = require('node-telegram-bot-api')
+
+const token = process.env.TELEGRAM_BOT_TOKEN
+  ? process.env.TELEGRAM_BOT_TOKEN
+  : ''
+
+const chatId = process.env.TELEGRAM_CHAT_ID ? process.env.TELEGRAM_CHAT_ID : ''
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: false })
+
+// Set some defaults (required if your JSON file is empty)
+
+async function start() {
+  await db.defaults({ posts: [] }).write()
+  console.log('==INITIALIZING DATABASE==')
+}
+async function init(username) {
+  ig.scrapeUserPage(username).then(result => {
+    const datas = result.medias
+    for (const data of datas) {
+      const { text = '' } = data
+      if (text.trim().length > 0) {
+        if (text.includes('46')) {
+          const check = db
+            .get('posts')
+            .find({ media_id: data.media_id })
+            .value()
+
+          if (!check) {
+            db.get('posts')
+              .push(data)
+              .write()
+
+            const generatedUrl = `https://www.instagram.com/p/${data.shortcode}/`
+            bot.sendMessage(chatId, data.text + '\n\n' + generatedUrl)
+          }
+        }
+      }
+    }
+  })
+}
+
+start()
+
+const stores = ['inestaku', 'ifootballstore']
+
+for (const store of stores) {
+  init(store)
+}
